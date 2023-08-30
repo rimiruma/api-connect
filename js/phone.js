38538040a954fb07bfb4db5926f5f24af4b4a92a
Rimi Ruma <rimiruma12@gmail.com>
2023-08-30 18:20:38 +0600

    four  commit

--- js/phone.js
+++ js/phone.js
@@ -14,7 +14,16 @@ const phoneContainer = document.getElementById('phone-container')
 // clear phone container cards before adding new cards
 phoneContainer.textContent = '';
 
-console.log(phones.length)
+// display show all button if there are more then 12 phomes
+const showAllContainer = document.getElementById('show-all-container')
+if(phones.length > 12){
+  showAllContainer.classList.remove('hidden')
+}
+else{
+  showAllContainer.classList.add('hidden')
+}
+// display only first 12 phones
+phones = phones.slice(0,12);
 
   phones.forEach(phone => {
     console.log(phone);
