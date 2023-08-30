5ec7afb2336e38d9f46b388be68acdb47fe5855f
Rimi Ruma <rimiruma12@gmail.com>
2023-08-30 16:39:07 +0600

    secound  commit

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
@@ -11,6 +11,8 @@ const displayPhone = phones => {
   // console.log(phones)
 
 const phoneContainer = document.getElementById('phone-container')
+// clear phone container cards before adding new cards
+phoneContainer.textContent = '';
 
   phones.forEach(phone => {
     console.log(phone);
@@ -39,9 +41,10 @@ const handleSearch = () =>{
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value
 console.log(searchText)
+loadPhone(searchText)
 }
 
 
 
 
-loadPhone()
+// loadPhone()
